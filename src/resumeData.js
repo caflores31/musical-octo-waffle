57aef1df7278e2c53423ab034dfba0ef65717599
Cherrie Awang <caflores31@gmail.com>
2021-05-26 17:15:54 -0700

    insert resume

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,10 +1,10 @@
 let resumeData = {
     "imagebaseurl":"https://caflores31.github.io/",
-    "name": "Cherrie Anne",
+    "name": "Cherrie",
     "role": "Developer Student and ServiceNow Consultant",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": "I am passionate about making open-source more accessible and building community.",
     "socialLinks":[
         {
           "name":"linkedin",
